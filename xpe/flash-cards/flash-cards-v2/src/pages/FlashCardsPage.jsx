79ec075c3bfe3e7f4384305d1beb02caaba8392e
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-07-05 22:04:13 -0300

    tentativa de colocar um loader no aplicativo

--- xpe/flash-cards/flash-cards-v2/src/pages/FlashCardsPage.jsx
+++ xpe/flash-cards/flash-cards-v2/src/pages/FlashCardsPage.jsx
@@ -4,6 +4,7 @@ import Button from '../components/Button';
 import FlashCard from '../components/FlashCard';
 import FlashCards from '../components/FlashCards';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import Main from '../components/Main';
 import RadioButton from '../components/RadioButton';
 import { helperShuffleArray } from '../helpers/arrayHelpers';
@@ -22,22 +23,24 @@ export default function FlashCardsPage() {
     // });
 
     //exemplo de async await:
-    // async function getAllCards() {
-    //   const backEndAllCards = await apiGetAllFlashCards();
-    //   setAllCards(backEndAllCards);
-    // }
-
-    // getAllCards();
-
-    //exemplo de IIFE:
-    (async function getAllCards() {
+    async function getAllCards() {
       const backEndAllCards = await apiGetAllFlashCards();
       setAllCards(backEndAllCards);
       setLoading(false);
-    })();
+    }
+
+    getAllCards();
   }, []);
 
-  function handleButtonClick() {
+  //exemplo de IIFE:
+  //   (async function getAllCards() {
+  //     const backEndAllCards = await apiGetAllFlashCards();
+  //     setAllCards(backEndAllCards);
+  //   })();
+
+  // getAllCards();
+
+  function handleShuffle() {
     const shuffledCards = helperShuffleArray(studyCards);
     setStudyCards(shuffledCards);
   }
@@ -75,11 +78,11 @@ export default function FlashCardsPage() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <Main />
       <div className="text-center mb-4">
-        <Button onButtonClick={handleButtonClick}>Embaralhar Cards</Button>
+        <Button onButtonClick={handleShuffle}>Embaralhar Cards</Button>
       </div>
       <div className="flex flex-row items-center justify-center space-x-4 m-4">
         <RadioButton
@@ -111,7 +114,7 @@ export default function FlashCardsPage() {
           />
         ))}
       </FlashCards>
-    </div>
+    </>
   );
 }
 
